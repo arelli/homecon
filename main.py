ee4d191f44a38f244023c358c4eda66d59243f90
Raf Ellinitakis <nickpap556@gmail.com>
2020-12-03 18:57:04 +0200

    Basic GUI Functionality
    still needs to have a proper view of stored receipts

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class receipt:
 			self.comments = input("Enter any comment about the transaction:             ")
 			self.id = input("Enter an id for the trasaction:                      ")
 
-
 	def print_info(self):
 		print("Amount:                " + str(self.amount))
 		print("Recipient:             " + str(self.recipient))
@@ -29,7 +28,6 @@ class receipt:
 		print("Method of Payment:     " + str(self.method))
 		print("Comments:              " + str(self.comments))
 		print("id Number:             " + str(self.id))
-
 
 class method_of_payment:
 	def __init__(self,name,comments,id):
@@ -50,7 +48,7 @@ class organisation:
 		self.comments = comments
 		self.id = 0
 
-
+# save an object of the receipt class as a text, in a specified file by appending it as csv
 def save_receipt(rcpt,filename):
 	line_to_save = str(rcpt.amount) + "," + str(rcpt.recipient) + "," + str(rcpt.sender) + "," + str(rcpt.date) + "," + str(rcpt.method) + "," + str(rcpt.comments) + "," + str(rcpt.id)
 	with open(filename, "a") as savefile:
@@ -72,66 +70,48 @@ def make_receipt(string):
 	temp_receipt.id = attr[6]
 	return temp_receipt
 
-
+# get list of income from file
 def get_income():
 	with open("income") as file:
 	    my_income = [line.strip() for line in file]
 	    file.close()
 	return my_income;
 
-
+# get list of expenses from file
 def get_expenses():
 	with open("expenses") as file:
 	    my_expenses = [line.strip() for line in file]
 	    file.close()
 	return my_expenses
 
-
+# get list of belongings from file
 def get_belongings():
 	with open("belongings") as file:
 	    my_belongings = [line.strip() for line in file]
 	    file.close()
 	return my_belongings
 
-
+# get list of methods from file
 def get_methods():
 	with open("methods") as file:
 	    list_of_methods = [line.strip() for line in file]
 	    file.close()
 	return list_of_methods
-
-
+# get list of persons from file
 def get_persons():
 	with open("persons") as file:
 	    list_of_persons = [line.strip() for line in file]
 	    file.close()
 	return list_of_persons
 
-
+#get list of organisations from file
 def get_oganisations():
 	with open("organisations") as file:
 	    list_of_organisations = [line.strip() for line in file]
 	    file.close()
 	return list_of_organisations
 
-
-def add_income():
-	print("Entering a new income receipt..")
-	temp_receipt = receipt()
-	temp_receipt.user_init()
-	print("Object created.Saving to file..\n")
-	save_receipt(temp_receipt,"income")
-	print("Done!\n")
-
-
-def add_expense():
-	print("Entering a new income receipt..")
-	temp_receipt = receipt()
-	temp_receipt.user_init()
-	print("Object created.Saving to file..\n")
-	save_receipt(temp_receipt,"expenses")
-	print("Done!\n")
-
+# print a list from a file
 def print_list(filename):
 		file = open(filename, "r")
 		line_list = [line.strip() for line in file]
@@ -145,6 +125,26 @@ def print_list(filename):
 			counter = counter + 1
 			print("\n")
 
+
+# add an income receipt from the terminal
+def add_income():
+	print("Entering a new income receipt..")
+	temp_receipt = receipt()
+	temp_receipt.user_init()
+	print("Object created.Saving to file..\n")
+	save_receipt(temp_receipt,"income")
+	print("Done!\n")
+
+#add an expence receipt from the terminal
+def add_expense():
+	print("Entering a new income receipt..")
+	temp_receipt = receipt()
+	temp_receipt.user_init()
+	print("Object created.Saving to file..\n")
+	save_receipt(temp_receipt,"expenses")
+	print("Done!\n")
+
+
 my_income = []
 my_expenses = []
 my_belongings = []
@@ -152,6 +152,8 @@ list_of_methods = []
 list_of_persons = []
 list_of_organisations = []
 
+
+# a one-window implementation of the add_receipt
 def graphical_add_receipt():
 	# handle button events
 	def press(button):
@@ -205,18 +207,37 @@ def graphical_add_receipt():
 	# start the GUI
 	receipt_graphical.go()
 
+# a main menu window that supports subwindows
 def menu_graphical():
 	def launch(win):
 		app.showSubWindow(win)
 
-	app=gui()
+	# method to house all the main_gui related buttons(such as exit)
+	def main_gui_press(button):  
+		if button == "Exit":
+			app.stop()
+		if button == "ViewReceipts":
+			print_list("expenses")
+			print_list("income")
+			app.showSubWindow("ViewReceipts")
+
+	app = gui("Homecon Manager", "500x400")
+	app.setBg("green")
+	app.setFont(12)
+	app.addLabel("title","Homecon Financial Manager")
+	app.setLabelBg("title","orange")
 
 	# these go in the main window
-	app.addButtons(["AddIncome", ""], launch)
+	app.addButtons(["AddReceipt"], launch)
+
+	app.addButtons(["ViewReceipts"], main_gui_press)
+
+	app.addButtons(["Exit"],main_gui_press)
+
 
 	#this has all the button press functionality for subWindow 1
-	def press_income_window(button):
-		if button == "Save":
+	def press_receipt_window(button):
+		if button == "SaveAsIncome":
 			temp_receipt = receipt()
 			temp_receipt.amount = app.getEntry("Amount")
 			temp_receipt.sender = app.getEntry("Sender")
@@ -227,9 +248,24 @@ def menu_graphical():
 			temp_receipt.id =  app.getEntry("Id")
 			save_receipt(temp_receipt,"income")
 			print("saved the income..\n")
+		if button == "SaveAsExpense":
+			temp_receipt = receipt()
+			temp_receipt.amount = app.getEntry("Amount")
+			temp_receipt.sender = app.getEntry("Sender")
+			temp_receipt.recipient = app.getEntry("Recipient")
+			temp_receipt.date = app.getEntry("Date")
+			temp_receipt.method = app.getEntry("Method")
+			temp_receipt.comments = app.getEntry("Comments") 
+			temp_receipt.id =  app.getEntry("Id")
+			save_receipt(temp_receipt,"expenses")
+			print("saved the expense..\n")
+
+		#the subwindows never really close. They can only be hidden after use
+		app.hideSubWindow("AddReceipt")
+
 
 	# this is a pop-up
-	app.startSubWindow("AddIncome", modal=True)
+	app.startSubWindow("AddReceipt", modal=True)
 	app.addLabel("l1", "Add a Receipt")
 	app.addLabelEntry("Amount")
 	app.addLabelEntry("Sender")
@@ -238,30 +274,21 @@ def menu_graphical():
 	app.addLabelEntry("Method")
 	app.addLabelEntry("Comments")
 	app.addLabelEntry("Id")
-	app.addButtons(["Save"], press_income_window)
+	app.addButtons(["SaveAsIncome", "SaveAsExpense"], press_receipt_window)
 	app.stopSubWindow()
 
+	app.startSubWindow("ViewReceipts", modal=True)
+	app.addMessage("output","They are displayed in the terminal for now!")
+	app.stopSubWindow()
 	
 	
 	app.go()
 
-
+# start the gui
 menu_graphical()
-"""  # The old menu
-choice = -1
 
-while(choice!="0"):
-	if (choice == "1"):
-		graphical_add_receipt()
-	elif(choice == "2"):
-		print_list("income")
-	elif(choice == "3"):
-		print_list("expenses")
-	else:
-		print("Please enter a valid option(0-3).\n")
 
-	choice = input("1:add receipt(income or expense), 2:print income, 3:print expenses, 0:exit:   ")
-"""
+
 
 
 
