899f6e8474e969923654d358a25c49175d158960
Raf Ellinitakis <nickpap556@gmail.com>
2020-12-04 22:16:02 +0200

    Added automatic uncompression

--- start.bat
+++ start.bat
@@ -1,2 +1,12 @@
+IF NOT EXIST appJar\NUL GOTO NOAPPJAR
 python main.py
 pause
+GOTO ENDOFSCRIPT
+:NOAPPJAR
+ECHO the folder appJar does not exist
+tar.exe -x -f appJar.zip
+echo uncompressing folder...
+python main.py
+pause
+
+:ENDOFSCRIPT
